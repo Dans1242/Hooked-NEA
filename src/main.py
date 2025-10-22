340359544fcca393c7d8e5c091b35667642a74a6
Dans1242 <danielszyja1242@gmail.com>
2025-10-22 11:25:59 +0100

    rendered the window

--- src/main.py
+++ src/main.py
@@ -1,3 +1,17 @@
 import pygame
 import random
 import json
+import pygame_gui
+
+# initialize pygame
+pygame.init()
+gamescreen = pygame.display.set_mode((900,550))
+# set window name to title
+pygame.display.set_caption("Hooked: Bestiary Odyssey")
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
