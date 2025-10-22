5da54f1111feec2359eff2eb1645705c224a0e84
Dans1242 <danielszyja1242@gmail.com>
2025-10-22 11:43:31 +0100

    added frame rate

--- src/main.py
+++ src/main.py
@@ -3,8 +3,13 @@ import random
 import json
 import pygame_gui
 
+
+#create a clock variable
+clock = pygame.time.Clock()
+
 # initialize pygame
 pygame.init()
+
 gamescreen = pygame.display.set_mode((900,550))
 # set window name to title
 pygame.display.set_caption("Hooked: Bestiary Odyssey")
@@ -15,3 +20,4 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    clock.tick(60)
