3683fa0a7eb950e1b6d1c4b20e1373eb84731f29
Dans1242 <danielszyja1242@gmail.com>
2025-10-22 11:52:38 +0100

    added input checking

--- src/main.py
+++ src/main.py
@@ -20,4 +20,11 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # check for when a key is pressed down and print which key was pressed down
+        if event.type == pygame.KEYDOWN()
+            print(f"Key pressed: {pygame.key.name(event.key)}")
+        # check which key was released and print it
+        if event.type == pygame.KEYUP()
+            print(f"Key released: {pygame.key.name(event.key)}")
+    
     clock.tick(60)
