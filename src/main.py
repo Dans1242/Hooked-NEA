6e841c95fd8c09492a0c306ef98d116ca6e8f4d4
Dans1242 <danielszyja1242@gmail.com>
2025-11-27 14:42:17 +0000

    working input and object between 2 files

--- src/main.py
+++ src/main.py
@@ -3,57 +3,33 @@ import random
 import json
 import pygame_gui
 
-clock = pygame.time.Clock()
+from player import playerClass
+
 pygame.init()
+clock = pygame.time.Clock()
 
 gamescreen = pygame.display.set_mode((900, 550))
 pygame.display.set_caption("Hooked: Bestiary Odyssey")
 
-class character(pygame.sprite.Sprite):
-    def __init__(self, image_path, x, y):
-        super().__init__()
-        self.image = pygame.image.load(image_path).convert_alpha()
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
 
-    def move(self, changeInX, changeInY):
-        self.rect.x += changeInX
-        self.rect.y += changeInY
+characterIdleRight = playerClass(r"C:\Users\User\Documents\Hooked-Bestiary-Odyssey\assets\sprites\characterIdleRight.png", 100, 100)
 
-
-playerSprite = character(
-    r"C:\Users\User\Documents\Hooked-Bestiary-Odyssey\assets\sprites\characterIdle.png",
-    100,
-    100
-)
-
-all_sprites = pygame.sprite.Group()
-all_sprites.add(playerSprite)
+characterSprites = pygame.sprite.Group()
+characterSprites.add(characterIdleRight)
 
 running = True
-speed = 6  # movement speed (pixels per frame)
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    wasd = pygame.key.get_pressed()
 
-    if wasd[pygame.K_w]:
-        playerSprite.move(0, -speed)
-    if wasd[pygame.K_s]:
-        playerSprite.move(0, speed)
-    if wasd[pygame.K_a]:
-        playerSprite.move(-speed, 0)
-    if wasd[pygame.K_d]:
-        playerSprite.move(speed, 0)
-
-    gamescreen.fill((0, 0, 0))          # clears the screen
-    all_sprites.draw(gamescreen)        # draws the sprite
-    pygame.display.flip()               # update the display
-
-    clock.tick(60) # set fps
+    playerClass.inputHandler(characterIdleRight)
+    
+    gamescreen.fill((0, 0, 0))
+    characterSprites.draw(gamescreen)
+    pygame.display.flip()
+    clock.tick(60)
 
 pygame.quit()
--- src/player.py
+++ src/player.py
@@ -1,2 +1,26 @@
 import pygame
 
+class playerClass(pygame.sprite.Sprite):
+    def __init__(self, image_path, x, y):
+        super().__init__()
+        self.image = pygame.image.load(image_path).convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = 5
+
+    def move(self, changeInX, changeInY):
+        self.rect.x += changeInX
+        self.rect.y += changeInY
+    
+    def inputHandler(self):
+        wasd = pygame.key.get_pressed()
+
+        if wasd[pygame.K_w]:
+            self.move(0, -self.speed)
+        if wasd[pygame.K_s]:
+            self.move(0, self.speed)
+        if wasd[pygame.K_a]:
+            self.move(-self.speed, 0)
+        if wasd[pygame.K_d]:
+            self.move(self.speed, 0)
