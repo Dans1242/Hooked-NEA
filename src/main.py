04def8b358b3613463229a43c486621fd5d6949a
Dans1242 <danielszyja1242@gmail.com>
2025-12-11 12:01:50 +0000

    back to starting point

--- src/main.py
+++ src/main.py
@@ -1,37 +1,24 @@
 import pygame
-import random
-import json
-import pygame_gui
-
-from player import playerClass
 
 pygame.init()
-clock = pygame.time.Clock()
 
 gamescreen = pygame.display.set_mode((900, 550))
 pygame.display.set_caption("Hooked: Bestiary Odyssey")
-
-
-characterIdleRight = playerClass(r"C:\Users\User\Documents\Hooked-Bestiary-Odyssey\assets\sprites\characterIdleRight.png", 100, 100)
-characterIdleLeft = playerClass(r"C:\Users\User\Documents\Hooked-Bestiary-Odyssey\assets\sprites\characterIdleLeft.png", 100, 100)
-
-characterSprites = pygame.sprite.Group()
-characterSprites.add(characterIdleRight)
-characterSprites.add(characterIdleLeft)
+clock = pygame.time.Clock()
 
 running = True
-
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            print(f"Key pressed: {pygame.key.name(event.key)}")
+        if event.type == pygame.KEYUP:
+            print(f"Key released: {pygame.key.name(event.key)}")
 
-
-    playerClass.inputHandler(characterIdleRight)
-    
     gamescreen.fill((0, 0, 0))
-    characterSprites.draw(gamescreen)
     pygame.display.flip()
     clock.tick(60)
+
 
 pygame.quit()
