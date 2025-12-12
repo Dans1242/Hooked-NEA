02901791e5a48cfb0fef2a6285ae2b08b694d375
Dans1242 <danielszyja1242@gmail.com>
2025-12-12 14:45:37 +0000

    updated rng

--- src/RNG.py
+++ src/RNG.py
@@ -3,15 +3,24 @@ import random
 
 bestiary = {
     "Salmon" : {"rarity": "Common", "chance": 0.45, "value": 5},
-    "Carp" : {"rarity": "Common", "chance": 0.4, "value": 5},
-    "Cod" : {"rarity": "Rare", "chance": 0.25, "value": 5},
-    "Tuna" : {"rarity": "Rare", "chance": 0.20, "value": 5},
-    "Crab" : {"rarity": "Epic", "chance": 0.05, "value": 5},
-    "Swordfish" : {"rarity": "Epic", "chance": 0.04, "value": 5},
-    "Jellyfish" : {"rarity": "Legendary", "chance": 0.006, "value": 5},
-    "Shark" : {"rarity": "Legendary", "chance": 0.006, "value": 5},
-    "Whale" : {"rarity": "Mythic", "chance": 0.004, "value": 5},
-    "Void Serpent" : {"rarity": "Secret", "chance": 0.001, "value": 5},
+    "Carp" : {"rarity": "Common", "chance": 0.4, "value": 6},
+    "Cod" : {"rarity": "Rare", "chance": 0.25, "value": 12},
+    "Tuna" : {"rarity": "Rare", "chance": 0.20, "value": 15},
+    "Crab" : {"rarity": "Epic", "chance": 0.05, "value": 45},
+    "Swordfish" : {"rarity": "Epic", "chance": 0.04, "value": 50},
+    "Jellyfish" : {"rarity": "Legendary", "chance": 0.006, "value": 160},
+    "Shark" : {"rarity": "Legendary", "chance": 0.006, "value": 170},
+    "Whale" : {"rarity": "Mythic", "chance": 0.004, "value": 500},
+    "Void Serpent" : {"rarity": "Secret", "chance": 0.001, "value": 2000},
 }
 
-fishCaught = random.choice()
+fishNames = list(bestiary.keys())
+fishRarities = [info["rarity"] for info in bestiary.values()]
+fishChances = [info["chance"] for info in bestiary.values()]
+
+fishRarityPairs = [(name, info["rarity"]) for name, info in bestiary.items()]
+
+def RNG():
+    return random.choice(fishNames, weights=fishChances)[0]
+catch = RNG()
+print(catch)
