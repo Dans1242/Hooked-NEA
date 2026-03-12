dbf11477f232d39c9310f9f06b8a4ed1ef2f023c
Dans1242 <danielszyja1242@gmail.com>
2026-03-12 16:13:24 +0000

    fixed main on pc

--- src/main.py
+++ src/main.py
@@ -7,21 +7,7 @@ from RNG import RNG
 from save_load import save_game, load_game, pickSave # imports functions needed for loading game, saving game, and picking/creating save slots
 from collision import blockedAreasBG1, blockedAreasBG2, checkCollision
 from ui import Button
-
-# Temporary loot tables listed below:
-
-#lootTable1 = {
-#    "Salmon" : {"rarity": "Common", "chance": 0.45, "value": 5},
-#    "Carp" : {"rarity": "Common", "chance": 0.4, "value": 6},
-#    "Cod" : {"rarity": "Rare", "chance": 0.25, "value": 12},
-#    "Tuna" : {"rarity": "Rare", "chance": 0.20, "value": 15},
-#    "Crab" : {"rarity": "Epic", "chance": 0.05, "value": 45},
-#    "Swordfish" : {"rarity": "Epic", "chance": 0.04, "value": 50},
-#    "Jellyfish" : {"rarity": "Legendary", "chance": 0.006, "value": 160},
-#    "Shark" : {"rarity": "Legendary", "chance": 0.006, "value": 170},
-#    "Whale" : {"rarity": "Mythic", "chance": 0.004, "value": 500},
-#    "Void Serpent" : {"rarity": "Secret", "chance": 0.001, "value": 2000},
-#}
+from collections import deque
 
 tempLootTable = {
     # Common – bread and butter fish
@@ -51,6 +37,16 @@ tempLootTable = {
 
     # Secret – flex items, not expected
     "Abyssal Serpent": {"rarity": "Secret", "chance": 0.001, "value": 2000}
+}
+
+raritySettings = {
+    "Common":    {"clicks": 3, "window": 3.0,  "colour": (255, 255, 255)},  # white
+    "Uncommon":  {"clicks": 3, "window": 2.5,  "colour": (0, 255, 0)},      # green
+    "Rare":      {"clicks": 3, "window": 2.0,  "colour": (0, 100, 255)},    # blue
+    "Epic":      {"clicks": 3, "window": 1.5,  "colour": (255, 192, 203)},    # pink
+    "Legendary": {"clicks": 3, "window": 1.0,  "colour": (255, 215, 0)},    # gold
+    "Mythic":    {"clicks": 3, "window": 0.8,  "colour": (128, 0, 128)},    # purple
+    "Secret":    {"clicks": 3, "window": 0.5,  "colour": (0, 0, 0)},    # black
 }
 
 pygame.init() # initializing the game
@@ -96,8 +92,23 @@ def play(chosenSave):
     shopMessage = ""
     shopResult = ""
     shopResultTime = 0
+
+    # - setting up fishing minigame variables - #
+    fishingState = "idle"
+    castTime = 0
+    minigameStartTime = 0
+    requiredClicks = 0
+    clickWindow = 0
+    clickCount = 0
+    pendingFish = None
+    exclamationColour = (255, 255, 255)
+    # - end of fishing variables #
+
+    fishMessages = deque()
+
     font = pygame.font.SysFont("arial", 24, bold=True) #large font for UI elements
     smallFont = pygame.font.SysFont("arial", 18) # smaller font for inventory items etc
+    exclamationFont = pygame.font.SysFont("arial", 75, bold=True)
 
     while running:
         for event in pygame.event.get():
@@ -108,24 +119,10 @@ def play(chosenSave):
             
             if event.type == pygame.KEYDOWN and event.key == pygame.K_e: #fishing
                     if bg2 and player.xPos > 300 and player.xPos < 600:  
-
-                        caughtFish = RNG(tempLootTable) # generates a fish to be caught based on the paramater (lootTable1) i.e. gives a fish from the lootTable1
-
-                        print("You caught a " + caughtFish[0] + "! Rarity: " + caughtFish[1] + ", Chance: " + caughtFish[2] + ", Value: " + str(caughtFish[3]) + " coins")
-                        fishName = caughtFish[0]
-
-                        # checks if the fish already exists in the inventory, if so adds 1 to the count
-                        if fishName in player.inventory: 
-                            player.inventory[fishName]["quantity"] += 1
-                        
-                        # if it doesnt exist it adds it along with the fish's info
-                        else:
-                            player.inventory[fishName] = {
-                                "quantity": 1,
-                                "rarity": caughtFish[1],
-                                "chance": caughtFish[2],
-                                "value": caughtFish[3]
-                            }
+                        if fishingState == "idle":
+                            fishingState = "casting"
+                            castTime = pygame.time.get_ticks()
+                            pendingFish = RNG(tempLootTable) # generates a fish to be caught based on the paramater (lootTable1) i.e. gives a fish from the lootTable1
                     else:
                         print("You can't fish here! Go to the pier to fish.")
             
@@ -155,6 +152,23 @@ def play(chosenSave):
                         shopMessage = ""
                         showShop = False
 
+            if event.type == pygame.MOUSEBUTTONDOWN: # checks for a mouse click
+                if fishingState == "minigame":
+                    clickCount += 1 # adds click everytime the player clicks
+                    if clickCount >= requiredClicks:
+                        fishingState = "idle" # completes the minigame and resets state
+                        fishName = pendingFish[0]
+                        fishMessages.append({"text": f"You caught a {fishName}!", "time": pygame.time.get_ticks()}) # adds caught fish and time it was caught to the list to be displayed
+                        if fishName in player.inventory: #inventory system
+                            player.inventory[fishName]["quantity"] += 1
+                        else:
+                            player.inventory[fishName] = {
+                                "quantity": 1,
+                                "rarity": pendingFish[1],
+                                "chance": pendingFish[2],
+                                "value": pendingFish[3]
+                            }
+                        pendingFish = None # resets pending fish after catching
 
 
 
@@ -200,6 +214,9 @@ def play(chosenSave):
             elif player.yVel < 0 and "up" in fd:
                 player.yPos -= player.yVel
 
+        
+        # -- DRAWING SETION -- #
+        
         if bg2:
             gamescreen.blit(background2, (0, 0))
             if debugMode:
@@ -214,7 +231,6 @@ def play(chosenSave):
             
         player.playerDraw(gamescreen) #draw player
         player.updateCollisionRect() #updates player collision box
-
 
         # coin balance display
         coinUI = font.render(f"Coins: {player.coins}", True, (255, 255, 0))
@@ -248,7 +264,51 @@ def play(chosenSave):
             else:
                 shopResult = "" # clear result after 2seconds
 
-        
+        if fishMessages:
+            while fishMessages and pygame.time.get_ticks() - fishMessages[0]["time"] > 2000: # current live time minus the time fish was caught
+                fishMessages.popleft() # removes the message from the queue
+            
+            yOffset = 0
+            for i in fishMessages:
+                messageText = smallFont.render(i["text"], True, (255, 255, 255)) # using small white text for the fish message
+                gamescreen.blit(messageText, (player.xPos, player.yPos - 30 - yOffset)) # draw the actial message
+                yOffset += 20 # offset for each message above eachother
+
+        if fishingState == "casting":
+            if pygame.time.get_ticks() - castTime > 500: # makes the player wait 5 seconds after casting before fish bites
+                fishingState = "warning"
+                minigameStartTime = pygame.time.get_ticks()
+                rarity = pendingFish[1] # fethecs rarity from the randomised fish
+                clickWindow = raritySettings[rarity]["window"] * 1000 # grabs click window depending on fish rarirty
+                requiredClicks = raritySettings[rarity]["clicks"] # grabs required clicks depending on fish rarity
+                clickCount = 0
+                exclamationColour = raritySettings[rarity]["colour"] # grabs the colour for the exclamation mark depending on rarity
+            else:
+                castText = font.render("Casting...", True, (255, 255, 255))
+                gamescreen.blit(castText, (player.xPos - 20, player.yPos - 30))
+
+        if fishingState == "warning":
+            exclamationText = exclamationFont.render("!", True, (exclamationColour))
+            gamescreen.blit(exclamationText, (player.xPos + 15, player.yPos - 60))
+            if pygame.time.get_ticks() - minigameStartTime > 2000: # ! lasts for 2 seconds
+                fishingState = "minigame"
+                minigameStartTime = pygame.time.get_ticks() #resets the timer for click window
+
+        if fishingState == "minigame":
+            timeLeft = clickWindow - (pygame.time.get_ticks() - minigameStartTime)# calculates time left for when the player can click
+            if timeLeft <= 0:
+                
+                # TIME RUNS OUT
+                fishingState = "idle"
+                fishMessages.append({"text": f"The {pendingFish[0]} got away...", "time": pygame.time.get_ticks()})
+                pendingFish = None
+            else:
+                # draw click counter and time left
+                clickText = smallFont.render(f"Clicks: {clickCount}/{requiredClicks}", True, (255, 255, 255))
+                timeText = smallFont.render(f"{timeLeft / 1000:.1f}s", True, (255, 255, 255))
+                gamescreen.blit(clickText, (player.xPos - 20, player.yPos - 70))
+                gamescreen.blit(timeText, (player.xPos - 20, player.yPos - 90))
+
         pygame.display.flip()
         clock.tick(60) # set FPS
 
